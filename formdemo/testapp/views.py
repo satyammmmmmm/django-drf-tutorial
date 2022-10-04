e128b2868594140f468dddf4e6a3ac83f310dd29
satyammmmmmmm <satyamgupta40@gmail.com>
2022-10-04 19:24:39 +0530

    validation

--- formdemo/testapp/views.py
+++ formdemo/testapp/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from testapp.forms import StudentForm
 # Create your views here.
 def student_input_view(request):
+    submitted=False
     form=StudentForm()
-    return render(request,'testapp/input.html',{'form':form})
+    if request.method=='POST':
+        form=StudentForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['marks'])
+            submitted=True
+        form=StudentForm()    
+    return render(request,'testapp/input.html',{'form':form},{'submitted':submitted})
